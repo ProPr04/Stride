caef34b2167e72ef0615efcca563ed4d63575932
Shreya Jadhav <shreyajjadhav428@gmail.com>
2026-08-21 19:26:58 +0530

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import './App.css';
 
 function App() {
+  const [currentView, setCurrentView] = useState('login');
+
   return (
     <div className="app-container">
-      <Login />
+      {currentView === 'login' ? (
+        <Login onSwitchToSignUp={() => setCurrentView('signup')} />
+      ) : (
+        <SignUp onSwitchToLogin={() => setCurrentView('login')} />
+      )}
     </div>
   );
 }
 
 export default App;
+
